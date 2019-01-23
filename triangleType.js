dc5064eff1715ebd38c10a8690bb330fda36a4a4
pcampbell-software <peter.campbell@mac.com>
2019-01-23 10:21:21 +0800

    Update triangleType.js

--- triangleType.js
+++ triangleType.js
@@ -7,29 +7,14 @@ exports.handler = async (event, context) => {
         let b = event.queryStringParameters.b;
         let c = event.queryStringParameters.c;
         
-        if (isInt(a) && isInt(b) && isInt(c)) {
+        if (this.isInt(a) && this.isInt(b) && this.isInt(c)) {
             if (a > 0 & b > 0 && c > 0) {
                 let occurences = {};
                 occurences[a] = a;
                 occurences[b] = b;
                 occurences[c] = c;
                 
-                switch (Object.keys(occurences).length) {
-                    case 1:
-                        resp = "Equilateral";
-                        break;
-                    case 2:
-                        let sides = [a,b,c].sort(function (a,b) {
-                             return b-a;
-                        });
-                        if (sides[1] + sides[2] > sides[0]) {
-                            resp = "Isosceles";
-                        }
-                        break;
-                    case 3:
-                        resp = "Scalene";
-                        break;
-                }
+                resp = exports.determineTriangle(occurences, a, b, c);
             }
         } else {
             resp = {"message":"The request is invalid."};
@@ -44,6 +29,26 @@ exports.handler = async (event, context) => {
     return response;
 };
 
-function isInt(value) {
+exports.determineTriangle = function(occurences, a, b, c) {
+    let resp = "";
+    switch (Object.keys(occurences).length) {
+        case 1:
+            resp = "Equilateral";
+            break;
+        case 2:
+            let sides = [a,b,c].sort(function (a,b) {
+                 return b-a;
+            });
+            if (sides[1] + sides[2] > sides[0]) {
+                resp = "Isosceles";
+            }
+            break;
+        case 3:
+            resp = "Scalene";
+            break;
+    }
+    return resp;
+}
+exports.isInt = function (value) {
 	return !isNaN(value) && parseInt(Number(value),10) == value;
 }
